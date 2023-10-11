bb8aeea75378763a5bfd02af8721d242d7df47e8
clauga <guerrero.ayala.cs@gmail.com>
2023-10-11 10:26:39 +0200

    quest SB-one to many

--- src/main/java/com/wildcodeschool/wildandwizard/entity/Wizard.java
+++ src/main/java/com/wildcodeschool/wildandwizard/entity/Wizard.java
@@ -1,9 +1,6 @@
 package com.wildcodeschool.wildandwizard.entity;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.sql.Date;
 
 @Entity
@@ -18,8 +15,18 @@ public class Wizard {
     private String birthPlace;
     private String biography;
     private boolean muggle;
+    @ManyToOne
+    private School school;
 
     public Wizard() {
+    }
+
+    public School getSchool() {
+        return school;
+    }
+
+    public void setSchool(School school) {
+        this.school = school;
     }
 
     public Long getId() {
